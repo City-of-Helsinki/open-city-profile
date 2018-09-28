cb3926579114a47d6a1762b520350c2143b7bb71
Igor Davydychev <igor.davydychev@gmail.com>
2018-09-28 12:13:44 +0300

    Test uploading image to profile
    Refs #12

--- profiles/tests/utils.py
+++ profiles/tests/utils.py
@@ -1,3 +1,8 @@
+from io import BytesIO
+
+from PIL import Image
+
+
 def get(api_client, url, status_code=200):
     response = api_client.get(url)
     assert response.status_code == status_code, 'Expected status code {} but got {} with data {}'.format(
@@ -24,3 +29,12 @@ def delete(api_client, url, status_code=204):
     assert response.status_code == status_code, 'Expected status code {} but got {} with data {}'.format(
         status_code, response.status_code, response.data)
     return response.data
+
+
+def create_in_memory_image_file(name='test_image', image_format='png', size=(512, 256), color=(128, 128, 128)):
+    image = Image.new('RGBA', size=size, color=color)
+    file = BytesIO()
+    file.name = '{}.{}'.format(name, image_format)
+    image.save(file, format=image_format)
+    file.seek(0)
+    return file
