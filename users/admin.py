d0b4abdaf638688256f3c3ca424046fca26c800f
Igor Davydychev <igor.davydychev@gmail.com>
2018-10-02 19:54:16 +0300

    Delete dead code
    All this code does not have any affect and is essentially dead.
    
    Refs #1

--- users/admin.py
+++ users/admin.py
@@ -8,31 +8,9 @@ User = get_user_model()
 
 @admin.register(User)
 class ExtendedUserAdmin(admin.ModelAdmin):
-    search_fields = ['username', 'uuid', 'email', 'first_name', 'last_name']
-    list_display = search_fields + ['is_active', 'is_staff', 'is_superuser']
     inlines = [ProfileAdmin]
-
-    def get_fieldsets(self, request, obj=None):
-        fieldsets = super(ExtendedUserAdmin, self).get_fieldsets(request, obj)
-        new_fieldsets = []
-        for (name, field_options) in fieldsets:
-            fields = list(field_options.get('fields', []))
-            if 'username' in fields:
-                fields.insert(fields.index('username'), 'uuid')
-                field_options = dict(field_options, fields=fields)
-            new_fieldsets.append((name, field_options))
-        return new_fieldsets
 
     def get_readonly_fields(self, request, obj=None):
         fields = super(ExtendedUserAdmin, self).get_readonly_fields(
             request, obj)
         return list(fields) + ['uuid']
-
-    def formfield_for_dbfield(self, db_field, request, **kwargs):
-        field = super(ExtendedUserAdmin, self).formfield_for_dbfield(
-            db_field, request, **kwargs)
-        if db_field.name == 'username':
-            # Allow username be filled from uuid in
-            # helusers.models.AbstractUser.clean
-            field.required = False
-        return field
