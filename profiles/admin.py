56f72ae6a3e2b731262fb043aa9957ab5b82484a
Igor Davydychev <igor.davydychev@gmail.com>
2018-10-05 13:47:35 +0300

    Limit divisions of interest in user's profile inline
    We should only show the divisions actually relevant for the
    current state of the project.

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
+from munigeo.models import AdministrativeDivision
 
 from profiles.models import Profile
 
 
 class ProfileAdmin(admin.StackedInline):
     model = Profile
+
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        if db_field.name == "divisions_of_interest":
+            kwargs["queryset"] = AdministrativeDivision.objects.filter(division_of_interest__isnull=False)
+        formfield = super().formfield_for_manytomany(db_field, request, **kwargs)
+        return formfield
