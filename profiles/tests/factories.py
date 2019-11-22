41f529eeb72de95c76f19feb7c68abeb30306bb4
Pekka Lampila <pekka@montel.fi>
2019-11-22 13:18:32 +0200

    Add missing factories

--- profiles/tests/factories.py
+++ profiles/tests/factories.py
@@ -2,7 +2,8 @@ import factory
 from thesaurus.models import Concept, Vocabulary
 
 from open_city_profile.tests.factories import UserFactory
-from profiles.models import Profile
+from profiles.consts import ADDRESS_TYPES, EMAIL_TYPES, PHONE_TYPES
+from profiles.models import Address, Email, Phone, Profile
 
 
 class ProfileFactory(factory.django.DjangoModelFactory):
@@ -10,6 +11,39 @@ class ProfileFactory(factory.django.DjangoModelFactory):
 
     class Meta:
         model = Profile
+
+
+class EmailFactory(factory.django.DjangoModelFactory):
+    profile = factory.SubFactory(ProfileFactory)
+    primary = False
+    email_type = EMAIL_TYPES[0][0]
+    email = factory.Faker("email")
+
+    class Meta:
+        model = Email
+
+
+class PhoneFactory(factory.django.DjangoModelFactory):
+    profile = factory.SubFactory(ProfileFactory)
+    primary = False
+    phone_type = PHONE_TYPES[0][0]
+    phone = factory.Faker("phone_number")
+
+    class Meta:
+        model = Phone
+
+
+class AddressFactory(factory.django.DjangoModelFactory):
+    profile = factory.SubFactory(ProfileFactory)
+    primary = False
+    address = factory.Faker("street_address")
+    postal_code = factory.Faker("postcode")
+    city = factory.Faker("city")
+    country_code = factory.Faker("country_code")
+    address_type = ADDRESS_TYPES[0][0]
+
+    class Meta:
+        model = Address
 
 
 class VocabularyFactory(factory.django.DjangoModelFactory):
