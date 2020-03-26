fdea0467f15635f6edaf223e53c8a8861db41380
Tuomas Haapala <tuomas.haapala@anders.fi>
2020-03-26 08:59:25 +0200

    Add SubscriptionType subscripted profile export

--- subscriptions/admin.py
+++ subscriptions/admin.py
@@ -1,13 +1,48 @@
 from adminsortable.admin import SortableAdmin
 from django.contrib import admin
+from django.db.models import Q
+from import_export import resources
+from import_export.admin import ExportActionMixin
+from import_export.fields import Field
 from parler.admin import TranslatableAdmin
+
+from profiles.models import Profile
 
 from .models import Subscription, SubscriptionType, SubscriptionTypeCategory
 
 
+class ProfileResource(resources.ModelResource):
+    email = Field()
+    phone = Field()
+
+    class Meta:
+        model = Profile
+        fields = ("email", "phone", "language")
+
+    def dehydrate_email(self, profile):
+        return (
+            profile.get_primary_email_value() if profile.newsletters_via_email else None
+        )
+
+    def dehydrate_phone(self, profile):
+        return (
+            profile.get_primary_phone_value() if profile.newsletters_via_sms else None
+        )
+
+
 @admin.register(SubscriptionType)
-class SubscriptionTypeAdmin(TranslatableAdmin):
-    pass
+class SubscriptionTypeAdmin(ExportActionMixin, TranslatableAdmin):
+    resource_class = ProfileResource
+
+    def get_export_data(self, file_format, queryset, *args, **kwargs):
+        profiles = (
+            Profile.objects.filter(
+                Q(newsletters_via_email=True) | Q(newsletters_via_sms=True)
+            )
+            .filter(subscriptions__subscription_type__in=queryset)
+            .distinct()
+        )
+        return super().get_export_data(file_format, profiles, *args, **kwargs)
 
 
 @admin.register(SubscriptionTypeCategory)
