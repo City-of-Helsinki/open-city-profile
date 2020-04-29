3ec9a9d3e5ddce11daf05b7222f14bad4eec66b3
Juha Louhiranta <juha.louhiranta@anders.fi>
2020-04-29 22:52:12 +0300

    Refactor seed_data and add new command line arguments
    New flags:
    - development, generate random fake profile data
      implies clear
    - clear, flush the db before initialization
    - munigeo, imports Helsinki divisions of interest
    
    Changed flags:
    - nosuperuser is changed to superuser i.e. superuser is only
      generated by seed_data when specifically requested
    
    Refs OM-753

--- utils/tests/test_commands.py
+++ utils/tests/test_commands.py
@@ -3,15 +3,28 @@ from django.core.management import call_command
 
 from profiles.models import Profile
 from services.enums import ServiceType
-from services.models import Service
+from services.models import AllowedDataField, Service
 from users.models import User
+from utils.utils import DATA_FIELD_VALUES
 from youths.models import YouthProfile
 
 
 def test_command_seed_data_works_without_arguments():
     call_command("seed_data")
+
     assert Service.objects.count() == len(ServiceType)
     assert Group.objects.count() == len(ServiceType)
+    assert User.objects.filter(is_superuser=True).count() == 0
+    assert AllowedDataField.objects.count() == len(DATA_FIELD_VALUES)
+
+
+def test_command_seed_data_initializes_development_data():
+    args = [
+        "--development",
+        "--superuser",
+    ]
+    call_command("seed_data", *args)
+
     anonymous_users = 1
     admin_users = 1
     normal_users = 50
@@ -21,17 +34,18 @@ def test_command_seed_data_works_without_arguments():
     )
     assert Profile.objects.count() == normal_users
     assert YouthProfile.objects.count() == 10
-    assert User.objects.filter(is_superuser=True).count() == 1
+    assert User.objects.filter(is_superuser=True).count() == admin_users
 
 
 def test_command_seed_data_works_withs_arguments():
     args = [
+        "--development",
         "--profilecount=20",
         "--youthprofilepercentage=0.5",
         "--locale=fi_FI",
-        "--nosuperuser",
+        "--superuser",
     ]
     call_command("seed_data", *args)
     assert Profile.objects.count() == 20
     assert YouthProfile.objects.count() == 10
-    assert User.objects.filter(is_superuser=True).count() == 0
+    assert User.objects.filter(is_superuser=True).count() == 1
