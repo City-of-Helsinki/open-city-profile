55e24237df8d996283a8b0661edeec811f350ae9
Juha Louhiranta <juha.louhiranta@anders.fi>
2020-05-04 11:53:08 +0300

    Add required subscription_type_category into SubscriptionTypeFactory
    Refs OM-753

--- subscriptions/tests/factories.py
+++ subscriptions/tests/factories.py
@@ -13,6 +13,7 @@ class SubscriptionTypeCategoryFactory(factory.django.DjangoModelFactory):
 class SubscriptionTypeFactory(factory.django.DjangoModelFactory):
     code = "TEST_NOTIFICATION"
     label = "Test notification"
+    subscription_type_category = factory.SubFactory(SubscriptionTypeCategoryFactory)
 
     class Meta:
         model = SubscriptionType
