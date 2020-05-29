7b28b3f71a89d713c78dac1a9f6803ef4c41811b
Juha Louhiranta <juha.louhiranta@anders.fi>
2020-05-29 09:59:06 +0300

    Add GDPR related delete functionality for Youth profile
    Delete also takes in `dry_run` parameters for checking
    if the youth profile can be deleted. Youth profile
    itself doesn't have restrictions for not allowing
    a delete, `dry_run` is included to implement the
    defined common GDPR API.
    
    Refs OM-524

--- youths/tests/test_gdpr_api.py
+++ youths/tests/test_gdpr_api.py
@@ -1,4 +1,10 @@
+import pytest
 from django.urls import reverse
+
+from youths.models import YouthProfile
+
+TRUE_VALUES = ["true", "True", "TRUE", "1", 1, True]
+FALSE_VALUES = ["false", "False", "FALSE", "0", 0, False]
 
 
 def test_disabled_gdpr_api_responds_with_404(settings, api_client, youth_profile):
@@ -21,3 +27,60 @@ def test_get_profile_information_from_gdpr_api(api_client, youth_profile, snapsh
 
     assert response.status_code == 200
     snapshot.assert_match(response.json())
+
+
+@pytest.mark.parametrize("true_value", TRUE_VALUES)
+def test_delete_profile_dry_run_data(true_value, api_client, youth_profile):
+    response = api_client.delete(
+        reverse("youths:gdpr", kwargs={"pk": youth_profile.profile.id}),
+        data={"dry_run": true_value},
+        format="json",
+    )
+
+    assert response.status_code == 204
+    assert YouthProfile.objects.count() == 1
+
+
+@pytest.mark.parametrize("true_value", TRUE_VALUES)
+def test_delete_profile_dry_run_query_params(true_value, api_client, youth_profile):
+    response = api_client.delete(
+        reverse("youths:gdpr", kwargs={"pk": youth_profile.profile.id})
+        + f"?dry_run={true_value}",
+    )
+
+    assert response.status_code == 204
+    assert YouthProfile.objects.count() == 1
+
+
+def test_delete_profile(api_client, youth_profile):
+    response = api_client.delete(
+        reverse("youths:gdpr", kwargs={"pk": youth_profile.profile.id})
+    )
+
+    assert response.status_code == 204
+    assert YouthProfile.objects.count() == 0
+
+
+@pytest.mark.parametrize("false_value", FALSE_VALUES)
+def test_delete_profile_dry_run_query_params_false(
+    false_value, api_client, youth_profile
+):
+    response = api_client.delete(
+        reverse("youths:gdpr", kwargs={"pk": youth_profile.profile.id})
+        + f"?dry_run={false_value}",
+    )
+
+    assert response.status_code == 204
+    assert YouthProfile.objects.count() == 0
+
+
+@pytest.mark.parametrize("false_value", FALSE_VALUES)
+def test_delete_profile_dry_run_data_false(false_value, api_client, youth_profile):
+    response = api_client.delete(
+        reverse("youths:gdpr", kwargs={"pk": youth_profile.profile.id}),
+        data={"dry_run": false_value},
+        format="json",
+    )
+
+    assert response.status_code == 204
+    assert YouthProfile.objects.count() == 0
--- youths/views.py
+++ youths/views.py
@@ -1,24 +1,71 @@
 from django.conf import settings
+from django.db import DatabaseError, transaction
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
-from rest_framework import status
+from rest_framework import serializers, status
+from rest_framework.exceptions import APIException
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from utils.models import SerializableMixin
 from youths.models import YouthProfile
+
+
+class DeletionNotAllowed(APIException):
+    status_code = 403
+    default_detail = "Profile cannot be deleted."
+    default_code = "deletion_not_allowed"
+
+
+class DryRunException(Exception):
+    """Indicate that request is being done as a dry run."""
+
+
+class DryRunSerializer(serializers.Serializer):
+    dry_run = serializers.BooleanField(required=False, default=False)
 
 
 class YouthProfileGDPRAPIView(APIView):
     # TODO: Add authentication and security
+
+    model = YouthProfile
 
     def dispatch(self, request, *args, **kwargs):
         if not settings.GDPR_API_ENABLED:
             return HttpResponse(status=404)
         return super().dispatch(request, *args, **kwargs)
 
-    def get_object(self) -> YouthProfile:
-        return get_object_or_404(YouthProfile, profile__pk=self.kwargs["pk"])
+    def get_object(self) -> SerializableMixin:
+        return get_object_or_404(self.model, profile__pk=self.kwargs["pk"])
+
+    def check_dry_run(self):
+        """Check if parameters provided to the view indicate it's being used for dry_run."""
+        data = DryRunSerializer(data=self.request.data)
+        query = DryRunSerializer(data=self.request.query_params)
+        data.is_valid()
+        query.is_valid()
+
+        if data.validated_data["dry_run"] or query.validated_data["dry_run"]:
+            raise DryRunException()
 
     def get(self, request, *args, **kwargs):
-        """Retrieve all youth profile data related to the given id."""
+        """Retrieve all profile data related to the given id."""
         return Response(self.get_object().serialize(), status=status.HTTP_200_OK)
+
+    def delete(self, request, *args, **kwargs):
+        """Delete all data related to the given profile.
+
+        Deletes all data related to the given profile id, or just checks if the data can be deleted,
+        depending on the `dry_run` parameter. Raises DeletionNotAllowed if the item
+        """
+        try:
+            with transaction.atomic():
+                self.get_object().delete()
+                self.check_dry_run()
+        except DryRunException:
+            # Deletion is possible. Due to dry run, transaction is rolled back.
+            pass
+        except DatabaseError:
+            raise DeletionNotAllowed()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
