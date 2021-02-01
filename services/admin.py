9490cfb1c3731ec770233461302fd43d54bb7569
Aki Koskinen <aki@secondlion.fi>
2021-02-01 10:56:43 +0200

    HP-342 Add service client ids to admin UI

--- services/admin.py
+++ services/admin.py
@@ -3,12 +3,19 @@ from django.contrib import admin
 from guardian.admin import GuardedModelAdmin
 from parler.admin import TranslatableAdmin
 
-from .models import AllowedDataField, Service, ServiceConnection
+from .models import AllowedDataField, Service, ServiceClientId, ServiceConnection
+
+
+class ServiceClientIdInline(admin.StackedInline):
+    model = ServiceClientId
+    extra = 0
+    verbose_name = "client id"
+    verbose_name_plural = "client ids"
 
 
 @admin.register(Service)
 class ServiceAdmin(TranslatableAdmin, GuardedModelAdmin):
-    pass
+    inlines = [ServiceClientIdInline]
 
 
 @admin.register(AllowedDataField)
