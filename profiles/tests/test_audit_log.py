4172aee3244e4aef353c6b7d24bb62b1b3862877
Aki Koskinen <aki@secondlion.fi>
2021-02-08 09:13:22 +0200

    HP-178 Replace deprecated Django log capture with Pytest's solution
    Django's patch_logger is documented as deprecated. Pytest offers a log
    capture fixture called caplog. It doens't work quite the same as
    Django's offering so there's a bit of filtering logic added on top of
    it.

--- profiles/tests/test_audit_log.py
+++ profiles/tests/test_audit_log.py
@@ -1,8 +1,8 @@
 import json
+import logging
 
 import pytest
 from django.conf import settings
-from django.test.utils import patch_logger
 
 from profiles.models import Profile
 
@@ -19,6 +19,18 @@ def enable_audit_log_username():
     settings.AUDIT_LOG_USERNAME = True
 
 
+@pytest.fixture
+def cap_audit_log(caplog):
+    def get_logs(self):
+        audit_records = [
+            r for r in self.records if r.name == "audit" and r.levelno == logging.INFO
+        ]
+        return [json.loads(r.getMessage()) for r in audit_records]
+
+    caplog.get_logs = get_logs.__get__(caplog)
+    return caplog
+
+
 def assert_common_fields(log_message):
     assert log_message["audit_event"]["origin"] == "PROFILE-BE"
     assert log_message["audit_event"]["status"] == "SUCCESS"
@@ -27,55 +39,61 @@ def assert_common_fields(log_message):
     assert log_message["audit_event"]["date_time"] is not None
 
 
-def test_audit_log_read(user, enable_audit_log):
+def test_audit_log_read(user, enable_audit_log, cap_audit_log):
     ProfileFactory()
-    with patch_logger("audit", "info") as cm:
-        profile = Profile.objects.first()
-        assert len(cm) == 1
-        log_message = json.loads(cm[0])
-        assert_common_fields(log_message)
-        assert log_message["audit_event"]["operation"] == "READ"
-        assert log_message["audit_event"]["target"] == {
-            "user_id": str(profile.user.uuid),
-            "profile_id": str(profile.pk),
-            "profile_part": "base profile",
-        }
+
+    cap_audit_log.clear()
+    profile = Profile.objects.first()
+    audit_logs = cap_audit_log.get_logs()
+    assert len(audit_logs) == 1
+    log_message = audit_logs[0]
+    assert_common_fields(log_message)
+    assert log_message["audit_event"]["operation"] == "READ"
+    assert log_message["audit_event"]["target"] == {
+        "user_id": str(profile.user.uuid),
+        "profile_id": str(profile.pk),
+        "profile_part": "base profile",
+    }
 
 
-def test_audit_log_update(user, enable_audit_log, profile):
-    with patch_logger("audit", "info") as cm:
-        profile.first_name = "John"
-        profile.save()
-        assert len(cm) == 1
-        log_message = json.loads(cm[0])
-        assert_common_fields(log_message)
-        assert log_message["audit_event"]["operation"] == "UPDATE"
-        assert log_message["audit_event"]["target"] == {
-            "user_id": str(profile.user.uuid),
-            "profile_id": str(profile.pk),
-            "profile_part": "base profile",
-        }
+def test_audit_log_update(user, enable_audit_log, profile, cap_audit_log):
+    profile.first_name = "John"
+    profile.save()
+    audit_logs = cap_audit_log.get_logs()
+    assert len(audit_logs) == 1
+    log_message = audit_logs[0]
+    assert_common_fields(log_message)
+    assert log_message["audit_event"]["operation"] == "UPDATE"
+    assert log_message["audit_event"]["target"] == {
+        "user_id": str(profile.user.uuid),
+        "profile_id": str(profile.pk),
+        "profile_part": "base profile",
+    }
 
 
-def test_audit_log_delete(user, enable_audit_log, profile):
-    with patch_logger("audit", "info") as cm:
-        profile.delete()
-        assert len(cm) == 1
-        log_message = json.loads(cm[0])
-        assert_common_fields(log_message)
-        assert log_message["audit_event"]["operation"] == "DELETE"
+def test_audit_log_delete(user, enable_audit_log, profile, cap_audit_log):
+    profile.delete()
+    audit_logs = cap_audit_log.get_logs()
+    assert len(audit_logs) == 1
+    log_message = audit_logs[0]
+    assert_common_fields(log_message)
+    assert log_message["audit_event"]["operation"] == "DELETE"
 
 
-def test_audit_log_create(user, enable_audit_log, enable_audit_log_username):
-    with patch_logger("audit", "info") as cm:
-        profile = ProfileFactory()
-        assert len(cm) == 2  # profile is accessed here as well, thus the 2 log entries
-        log_message = json.loads(cm[1])
-        assert_common_fields(log_message)
-        assert log_message["audit_event"]["operation"] == "CREATE"
-        assert log_message["audit_event"]["target"] == {
-            "user_id": str(profile.user.uuid),
-            "user_name": profile.user.username,
-            "profile_id": str(profile.pk),
-            "profile_part": "base profile",
-        }
+def test_audit_log_create(
+    user, enable_audit_log, enable_audit_log_username, cap_audit_log
+):
+    profile = ProfileFactory()
+    audit_logs = cap_audit_log.get_logs()
+    assert (
+        len(audit_logs) == 2
+    )  # profile is accessed here as well, thus the 2 log entries
+    log_message = audit_logs[1]
+    assert_common_fields(log_message)
+    assert log_message["audit_event"]["operation"] == "CREATE"
+    assert log_message["audit_event"]["target"] == {
+        "user_id": str(profile.user.uuid),
+        "user_name": profile.user.username,
+        "profile_id": str(profile.pk),
+        "profile_part": "base profile",
+    }
