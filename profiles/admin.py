ec5cea0089ae35272c01d80b1700adf7d27228c5
Aki Koskinen <aki@secondlion.fi>
2021-02-11 12:44:34 +0200

    HP-521 Replace partial Profile data with a link to the complete Profile
    The full Profile associated with a User can now be easily accessed from
    the User admin page.
    
    Django doesn't provide out of the box solution to this kind of links, so
    have to craft some HTML by hand.

--- profiles/admin.py
+++ profiles/admin.py
@@ -209,15 +209,3 @@ class ExtendedProfileAdmin(VersionAdmin):
             )
         formfield = super().formfield_for_manytomany(db_field, request, **kwargs)
         return formfield
-
-
-class ProfileAdminInline(admin.StackedInline):
-    model = Profile
-
-    def formfield_for_manytomany(self, db_field, request, **kwargs):
-        if db_field.name == "divisions_of_interest":
-            kwargs["queryset"] = AdministrativeDivision.objects.filter(
-                division_of_interest__isnull=False
-            )
-        formfield = super().formfield_for_manytomany(db_field, request, **kwargs)
-        return formfield
--- users/admin.py
+++ users/admin.py
@@ -3,9 +3,10 @@ from copy import deepcopy
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.urls import reverse
+from django.utils.html import format_html
+from django.utils.translation import gettext_lazy as _
 from reversion.admin import VersionAdmin
-
-from profiles.admin import ProfileAdminInline
 
 User = get_user_model()
 
@@ -20,16 +21,25 @@ class UserAdmin(VersionAdmin, DjangoUserAdmin):
         "is_staff",
     )
     search_fields = ("uuid", "first_name", "last_name", "email")
-    inlines = [ProfileAdminInline]
 
     def get_fieldsets(self, request, obj=None):
         fieldsets = super().get_fieldsets(request, obj)
         if obj:
             fieldsets = deepcopy(fieldsets)
-            fieldsets[1][1]["fields"] += ("uuid",)
+            fieldsets[1][1]["fields"] += ("uuid", "get_profile_uuid_link")
             fieldsets[2][1]["fields"] += ("department_name", "ad_groups")
         return fieldsets
 
     def get_readonly_fields(self, request, obj=None):
         fields = super().get_readonly_fields(request, obj)
-        return list(fields) + ["uuid"]
+        return list(fields) + ["uuid", "get_profile_uuid_link"]
+
+    def get_profile_uuid_link(self, obj):
+        profile_id = obj.profile.id
+        profile_url = reverse("admin:profiles_profile_change", args=(profile_id,))
+        hint = _("See Profile")
+        return format_html(
+            '<a href="{}" title="{}">{}</a>', profile_url, hint, profile_id
+        )
+
+    get_profile_uuid_link.short_description = _("Profile")
