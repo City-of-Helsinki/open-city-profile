277ad95e5f18d0c40d214584e5a3877672d16acd
Aki Koskinen <aki@secondlion.fi>
2021-02-24 09:13:13 +0200

    Revert "HP-556 Change audit logging to "buffered" type"
    This reverts commit 8f96d1573acb4339b5b067aba959f309b9065e49.
    
    The buffered audit logging introduced more serious problems than it
    fixed. The previous version caused some performance issues. The buffered
    version might cause audit logging to fail, when models are deleted, as
    they might not be present in the database anymore at the time of
    logging.

--- profiles/log_signals.py
+++ profiles/log_signals.py
@@ -1,22 +1,22 @@
 from django.db.models.signals import post_delete, post_init, post_save
 from django.dispatch import receiver
 
-from .audit_log import add_loggable
+from .audit_log import log
 
 
 @receiver(post_delete)
 def post_delete_audit_log(sender, instance, **kwargs):
-    add_loggable("DELETE", instance)
+    log("DELETE", instance)
 
 
 @receiver(post_init)
 def post_init_audit_log(sender, instance, **kwargs):
-    add_loggable("READ", instance)
+    log("READ", instance)
 
 
 @receiver(post_save)
 def post_save_audit_log(sender, instance, created, **kwargs):
     if created:
-        add_loggable("CREATE", instance)
+        log("CREATE", instance)
     else:
-        add_loggable("UPDATE", instance)
+        log("UPDATE", instance)
--- profiles/middleware.py
+++ profiles/middleware.py
@@ -1,6 +1,5 @@
 from django.utils.deprecation import MiddlewareMixin
 
-from .audit_log import flush_audit_log
 from .utils import clear_thread_locals, set_current_request
 
 
@@ -9,6 +8,5 @@ class SetCurrentRequest(MiddlewareMixin):
         set_current_request(request)
 
     def process_response(self, request, response):
-        flush_audit_log()
         clear_thread_locals()
         return response
