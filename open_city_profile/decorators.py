df8176c15594983c07f14773c886e6d0d4a19fb0
Aki Koskinen <aki@secondlion.fi>
2021-07-01 13:58:11 +0300

    HP-843 Implement helper for building decorators
    The decorators have some common boilerplate that was repeated on each of
    them. Extracted that into a builder function. Now adding a new decorator
    is a simple task.

--- open_city_profile/decorators.py
+++ open_city_profile/decorators.py
@@ -7,67 +7,72 @@ from graphql.execution.base import ResolveInfo
 from open_city_profile.exceptions import ServiceNotIdentifiedError
 
 
-def context_helper(f):
-    # helper decorator to get the context for the actual decorator
-    def decorator(func):
-        def wrapper(*args, **kwargs):
-            info = next(arg for arg in args if isinstance(arg, ResolveInfo))
-            return func(info.context, *args, **kwargs)
+def _use_context_tests(*test_funcs):
+    def decorator(decorator_function):
+        @wraps(decorator_function)
+        def wrapper(function):
+            @wraps(function)
+            def context_tester(*args, **kwargs):
+                info = next(arg for arg in args if isinstance(arg, ResolveInfo))
+                context = info.context
+
+                for test_func in test_funcs:
+                    test_func(context)
+
+                return function(*args, **kwargs)
+
+            return context_tester
 
         return wrapper
 
     return decorator
 
 
-def login_and_service_required(function):
-    """Decorator for checking that the user is logged in and service is known"""
+def _require_authenticated(context):
+    if not context.user.is_authenticated:
+        raise PermissionDenied(_("You do not have permission to perform this action."))
 
-    @wraps(function)
-    @context_helper(function)
-    def wrapper(context, *args, **kwargs):
-        if not context.user.is_authenticated:
+
+def _require_service(context):
+    if not context.service:
+        raise ServiceNotIdentifiedError("No service identified")
+
+
+def _require_service_permission(permission_name):
+    def permission_checker(context):
+        _require_service(context)
+
+        if not context.user.has_perm(permission_name, context.service):
             raise PermissionDenied(
                 _("You do not have permission to perform this action.")
             )
 
-        if not context.service:
-            raise ServiceNotIdentifiedError("No service identified")
+    return permission_checker
 
-        return function(*args, **kwargs)
 
-    return wrapper
+@_use_context_tests(_require_authenticated, _require_service)
+def login_and_service_required():
+    """Decorator for checking that the user is logged in and service is known"""
 
 
 def staff_required(required_permission="view"):
     """
-    Decorator for checking if user has staff permission on profiles for service
+    Returns a decorator that checks if user has staff permission on profiles for service
     specified in the request. Required permission can be defined as an argument,
     defaults to 'view'.
     """
 
-    def method_wrapper(function):
-        @wraps(function)
-        @context_helper(function)
-        def wrapper(context, *args, **kwargs):
-            if required_permission not in ("view", "manage"):
-                raise ValueError(
-                    "Invalid required_permission given as argument: '{}'".format(
-                        required_permission
-                    )
-                )
+    if required_permission not in ("view", "manage"):
+        raise ValueError(
+            "Invalid required_permission given as argument: '{}'".format(
+                required_permission
+            )
+        )
 
-            if not context.service:
-                raise ServiceNotIdentifiedError(_("No service identified"))
+    @_use_context_tests(
+        _require_service_permission("can_{}_profiles".format(required_permission))
+    )
+    def check_permission():
+        f"""Decorator that checks for can_{required_permission}_profiles permission."""
 
-            if context.user.has_perm(
-                "can_{}_profiles".format(required_permission), context.service
-            ):
-                return function(*args, **kwargs)
-            else:
-                raise PermissionDenied(
-                    _("You do not have permission to perform this action.")
-                )
-
-        return wrapper
-
-    return method_wrapper
+    return check_permission
