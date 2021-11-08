c532292628c67812b15e002d0032b4feb39bf4f9
Aki Koskinen <aki@secondlion.fi>
2021-11-08 15:51:46 +0200

    HP-758 Remove django-ilmoitin notification types
    The only notification types were related to the legal relationship
    concept. Since there are no more notification types, effectively
    django-ilmoitin isn't used any more.

--- profiles/enums.py
+++ profiles/enums.py
@@ -2,17 +2,6 @@ from django.utils.translation import ugettext_lazy as _
 from enumfields import Enum
 
 
-class NotificationType(Enum):
-    RELATIONSHIP_CONFIRMATION_NEEDED = "relationship_confirmation_needed"
-    RELATIONSHIP_CONFIRMED = "relationship_confirmed"
-
-    class Labels:
-        RELATIONSHIP_CONFIRMATION_NEEDED = _(
-            "Legal relationship created, confirmation needed"
-        )
-        RELATIONSHIP_CONFIRMED = _("Legal relationship confirmed")
-
-
 class RepresentationType(Enum):
     CUSTODY = "custody"
 
