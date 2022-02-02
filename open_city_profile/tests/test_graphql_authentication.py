31d521ba361c8cf899e4266d55f8eb832eec45ad
Mikko Keskinen <mikko.keskinen@hel.fi>
2022-02-02 13:50:02 +0200

    HP-943 Expose JWT authentication errors to the client
    The "GENERAL_ERROR" error code returned to the client in case of
    an AuthenticationError raised in helusers RequestJWTAuthentication
    authenticator was deemed to be too generic.
    
    This commit adds a new error code "JWT_AUTHENTICATION_ERROR" to make
    it easier for the client to detect those kinds of errors.

--- open_city_profile/tests/test_graphql_authentication.py
+++ open_city_profile/tests/test_graphql_authentication.py
@@ -1,4 +1,6 @@
-from .graphql_test_helpers import BearerTokenAuth, do_graphql_call
+from helusers.models import OIDCBackChannelLogoutEvent
+
+from .graphql_test_helpers import BearerTokenAuth, do_graphql_call, generate_jwt_token
 
 
 def test_not_presenting_an_access_token_with_operation_needing_authentication_returns_permission_denied_error(
@@ -17,11 +19,11 @@ def test_not_presenting_an_access_token_with_operation_needing_authentication_re
     assert type(data["_service"]["sdl"]) is str
 
 
-def test_presenting_an_expired_access_token_with_any_operation_returns_general_error(
+def test_presenting_an_expired_access_token_with_any_operation_returns_jwt_authentication_error(
     unix_timestamp_now, live_server,
 ):
     claims = {"exp": unix_timestamp_now - 1}
-    data, errors = do_graphql_call(live_server, BearerTokenAuth(extra_claims=claims),)
+    data, errors = do_graphql_call(live_server, BearerTokenAuth(extra_claims=claims))
 
     assert len(errors) == 2
 
@@ -29,4 +31,27 @@ def test_presenting_an_expired_access_token_with_any_operation_returns_general_e
         assert data[path] is None
 
         error = next(err for err in errors if err["path"] == [path])
-        assert error["extensions"]["code"] == "GENERAL_ERROR"
+        assert error["extensions"]["code"] == "JWT_AUTHENTICATION_ERROR"
+
+
+def test_presenting_a_logged_out_token_returns_jwt_authentication_error(live_server,):
+    jwt_data, encoded_jwt_token = generate_jwt_token()
+
+    OIDCBackChannelLogoutEvent.objects.create(
+        iss=jwt_data["iss"], sid=jwt_data["sid"], sub=jwt_data["sub"],
+    )
+
+    data, errors = do_graphql_call(
+        live_server,
+        extra_request_args={
+            "headers": {"Authorization": f"Bearer {encoded_jwt_token}"},
+        },
+    )
+
+    assert len(errors) == 2
+
+    for path in ["myProfile", "_service"]:
+        assert data[path] is None
+
+        error = next(err for err in errors if err["path"] == [path])
+        assert error["extensions"]["code"] == "JWT_AUTHENTICATION_ERROR"
