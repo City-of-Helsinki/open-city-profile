9ceaf77f6bacd9ee80e79f2497265f9bff3b0782
Aki Koskinen <aki@secondlion.fi>
2022-05-12 12:01:18 +0300

    HP-1409 Change middleware to new style
    The MiddlewareMixin is deprecated - even its module name says so.

--- profiles/middleware.py
+++ profiles/middleware.py
@@ -1,12 +1,15 @@
-from django.utils.deprecation import MiddlewareMixin
-
 from .utils import clear_thread_locals, set_current_request
 
 
-class SetCurrentRequest(MiddlewareMixin):
-    def process_request(self, request):
+class SetCurrentRequest:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
         set_current_request(request)
 
-    def process_response(self, request, response):
+        response = self.get_response(request)
+
         clear_thread_locals()
+
         return response
