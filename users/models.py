c87de0728361b49d5555b8615c7d969822384cc5
Aki Koskinen <aki@secondlion.fi>
2022-11-29 10:25:55 +0200

    HP-1429 Add is_system_user boolean field to User model
    This field is meant to be used to distinguish "system" users from human
    users. A system user represents some third party system that may want to
    authenticate to open-city-profile.

--- users/models.py
+++ users/models.py
@@ -1,5 +1,13 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 from helusers.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    is_system_user = models.BooleanField(
+        _("system user status"),
+        default=False,
+        help_text=_(
+            "Designates that this user represents another system instead of a human being."
+        ),
+    )
