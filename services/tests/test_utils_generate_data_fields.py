da56224a09fc8683c7a626ffc348319a3e1c4c3c
Aki Koskinen <aki@secondlion.fi>
2023-01-17 15:59:28 +0200

    Add test for adding a new allowed data field
    The implementation already did this, there just wasn't a test for it.
    
    Refactored the tests so that there's a helper function that does the
    actual testing. The test functions only have to provide the updated
    specification for the allowed data fields.

--- services/tests/test_utils_generate_data_fields.py
+++ services/tests/test_utils_generate_data_fields.py
@@ -1,12 +1,9 @@
-import pytest
-
 from services.models import AllowedDataField
 from services.utils import generate_data_fields
 
 
-@pytest.mark.parametrize("times", [1, 2])
-def test_generate_data_fields(times):
-    allowed_data_fields_spec = [
+def _get_initial_spec():
+    return [
         {
             "field_name": "name",
             "translations": [
@@ -24,16 +21,36 @@ def test_generate_data_fields(times):
         },
     ]
 
-    """Test data fields are generated and that function can be run multiple times."""
-    assert AllowedDataField.objects.count() == 0
-    for i in range(times):
-        generate_data_fields(allowed_data_fields_spec)
-    allowed_data_fields = AllowedDataField.objects.all()
-    assert len(allowed_data_fields) == len(allowed_data_fields_spec)
 
-    for value in allowed_data_fields_spec:
-        field = allowed_data_fields.filter(field_name=value["field_name"]).first()
-        assert field is not None
-        for translation in value["translations"]:
-            field.set_current_language(translation["code"])
-            assert field.label == translation["label"]
+def _test_changed_spec(new_spec):
+    for spec in _get_initial_spec(), new_spec:
+        generate_data_fields(spec)
+
+        allowed_data_fields = AllowedDataField.objects.all()
+        assert len(allowed_data_fields) == len(spec)
+
+        for value in spec:
+            field = allowed_data_fields.filter(field_name=value["field_name"]).first()
+            assert field is not None
+            for translation in value["translations"]:
+                field.set_current_language(translation["code"])
+                assert field.label == translation["label"]
+
+
+def test_unchanged_configuration_changes_nothing():
+    _test_changed_spec(_get_initial_spec())
+
+
+def test_adding_a_new_field():
+    new_spec = _get_initial_spec() + [
+        {
+            "field_name": "address",
+            "translations": [
+                {"code": "en", "label": "Address"},
+                {"code": "fi", "label": "Osoite"},
+                {"code": "sv", "label": "Adress"},
+            ],
+        }
+    ]
+
+    _test_changed_spec(new_spec)
