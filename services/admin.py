9fc74b0c40475fb6f21a9a47019f2247f2a9ed5e
Matti Eiden <matti.eiden@anders.com>
2024-01-17 09:54:54 +0200

    chore: improve admin service listing
    Adds new columns, filters and makes services more searchable.
    
    Ref HP-2169

--- services/admin.py
+++ services/admin.py
@@ -15,27 +15,79 @@ class ServiceClientIdInline(admin.StackedInline):
     verbose_name_plural = "client ids"
 
 
+class AllowedDataFieldsFilter(admin.SimpleListFilter):
+    title = _("allowed data fields")
+    parameter_name = "empty"
+
+    def lookups(self, request, model_admin):
+        names = (
+            AllowedDataField.objects.all()
+            .order_by("field_name")
+            .values_list("field_name", flat=True)
+        )
+        return [
+            ("empty", _("Empty")),
+        ] + [(name, name) for name in names]
+
+    def queryset(self, request, queryset):
+        value = self.value()
+        if value == "empty":
+            return queryset.filter(allowed_data_fields__isnull=True)
+        elif value:
+            return queryset.filter(allowed_data_fields__field_name=self.value())
+        return queryset
+
+
 @admin.register(Service)
 class ServiceAdmin(TranslatableAdmin, GuardedModelAdmin):
-    list_display = ("indicate_profile_service",)
+    list_display = (
+        "indicate_profile_service",
+        "_allowed_data_fields",
+        "_client_ids",
+        "idp",
+        "_gdpr",
+    )
+    list_filter = (AllowedDataFieldsFilter, "idp")
+    search_fields = (
+        "name",
+        "translations__title",
+        "client_ids__client_id",
+    )
+    ordering = ("name",)
+
     inlines = [ServiceClientIdInline]
 
+    @admin.display(description=str(Service._meta.verbose_name))
     def indicate_profile_service(self, obj):
-        result = str(obj)
         if obj.is_profile_service:
-            result = format_html(
-                '<span title="{}">⚙️ {}</span>', _("Profile service"), result
+            return format_html(
+                '<span title="{}">⚙️ {}</span>', _("Profile service"), obj.name
             )
-        return result
+        return obj.name
 
-    indicate_profile_service.short_description = str(Service._meta.verbose_name)
+    def _allowed_data_fields(self, obj):
+        return ", ".join(
+            [data_field.field_name for data_field in obj.allowed_data_fields.all()]
+        )
+
+    def _client_ids(self, obj):
+        return ", ".join([client_id.client_id for client_id in obj.client_ids.all()])
+
+    @admin.display(boolean=True)
+    def _gdpr(self, obj):
+        keycloak_ok = bool(obj.gdpr_audience) if obj.is_pure_keycloak else True
+        return (
+            bool(obj.gdpr_url)
+            and bool(obj.gdpr_query_scope)
+            and bool(obj.gdpr_delete_scope)
+            and keycloak_ok
+        )
 
     def get_fieldsets(self, request, obj=None):
         fieldsets = [
             (_("Translatable texts"), {"fields": ("title", "description")}),
             (_("Common options"), {"fields": ("name", "allowed_data_fields")}),
         ]
-
         if obj is None or not obj.is_profile_service:
             fieldsets.append(
                 (
