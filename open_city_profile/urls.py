b8655714bf882d72efc9f91333198325ccf6e655
Tero Virtanen <tero.virtanen@mavericks.fi>
2024-03-28 11:57:23 +0200

    fix: url.py import JsonResponse

--- open_city_profile/urls.py
+++ open_city_profile/urls.py
@@ -1,9 +1,7 @@
-import json
-
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.urls import include, path
 from django.utils.translation import gettext_lazy as _
 from django.views.decorators.csrf import csrf_exempt
